f4b716e80cbdff19dcc2d00f55edd9f87bd0ca50
Python Developer <PythonDeveloper@gmail.com>
2022-12-01 19:28:56 +0530

    Our First Python Program

---
+++ Day 4 (Our First Program)/main.py
@@ -0,0 +1,14 @@
+# print() function is used to print in console.
+# Syntax - print("statement")
+# Example :-
+print("Hello World")
+
+# We can also perform some computation using print().
+# Example :-
+print(5 + 6)
+print(10 - 2)
+
+
+# To run this program, make sure you have already installed python in your system.
+# If you have installed IDE or Text Editor you can easily run this program.
+# You can run python program using command prompt also, first open command prompt in that location and type python filename.py (replace filename.py to your python file), your program will run.
