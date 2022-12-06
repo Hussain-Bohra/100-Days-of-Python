8af52e415f8562a5b37d9d5cf9e4e6070ede80ae
Python Developer <PythonDeveloper@gmail.com>
2022-12-06 21:48:09 +0530

    Exercise - 1 Added

---
+++ Day 8 (Exercise 1 - Create a Calculator)/Exercise 1 - Create a Calculator.py
@@ -0,0 +1,15 @@
+# Here We Do Our First Exercise :-
+
+# Exercise - Write a Python Program to Create a Calculator using Arithmetic Operators. 
+
+num1 = 8
+num2 = 2
+
+print("Addition = ",num1 + num2)
+print("Subtraction = ",num1 - num2)
+print("Multiplication = ",num1 * num2)
+print("Division = ",num1 / num2)
+print("Floor Division = ",num1 // num2)
+print("Remainder = ",num1 % num2)
+print("Exponential = ",num1 ** num2)
+
