b5df3b5f41c1d064c035d9674e648275202d9698
Python Developer <PythonDeveloper@gmail.com>
2022-12-07 21:11:10 +0530

    Type Casting or Conversion in Python

---
+++ Day 9 (Type Casting in Python)/implicit_And_Explicit_Type_Casting.py
@@ -0,0 +1,18 @@
+print("Implicit Type Casting Example :-")
+print("Integer converting to Float numbers automatically by Python!")
+a = 2 # a is a integer
+b = 4.5 # b is a float
+
+print(a + b) # result will be float
+
+print()
+
+print("Explicit Type Casting Example :-")
+print("We are converting String to Integer Manually!")
+a = "5" # a is a string
+b = "10" # b is a string
+
+print(int(a) + int(b)) # result will be integer
+
+
+
