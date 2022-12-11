c6242f19e7c4b29c499b4849e721b053052755ea
Python Developer <PythonDeveloper@gmail.com>
2022-12-11 23:38:09 +0530

    What is Strings?

---
+++ Day 11 (Strings in Python)/string practical.py
@@ -0,0 +1,36 @@
+# Simple String Example :-
+# name = "Brock"
+# print("Hello, ",name)
+
+
+# Display Single Quote or Double Quotes in an Output of a String :-
+# Example of Displaying Single Quote in a String :-
+# print("Hello, How's Your Coding Journey.")
+
+# Example of Displaying Double Quote in a String :-
+# print('You Know, "Brock Lesnar" is a Beast.')
+
+
+# Multi-line String Example :-
+# We Use triple (Single or Double) Quotes for Multi-line String.
+# str = '''Computer programming is the process of performing a particular computation, usually by designing and building an executable computer program. Programming involves tasks such as analysis, generating algorithms, profiling algorithms' accuracy and resource consumption, and the implementation of algorithm. '''
+# print("Define Computer Programming:",str)
+
+
+# Accessing Characters of string one by one using its index number.
+# Example :-
+# text = "Brock"
+# print(text[0])
+# print(text[1])
+# print(text[2])
+# print(text[3])
+# print(text[4])
+
+
+
+# Accessing Characters of string one by one using for loop.
+# Example :-
+myname = "Hello World"
+for characters in myname:
+    print(characters)
+
