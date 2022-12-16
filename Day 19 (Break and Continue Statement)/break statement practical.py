ed0a48e41e31f26bfa1c08611bab38b9fa5b1e13
Python Developer <PythonDeveloper@gmail.com>
2022-12-16 18:58:08 +0530

    Break and Continue Statement

---
+++ Day 19 (Break and Continue Statement)/break statement practical.py
@@ -0,0 +1,9 @@
+# break statement Example :-
+for i in range(1,10):
+    print(i)
+
+    if(i == 5):
+        # when i value becomes 5, it will break the loop.
+        break
+
+print("Program Execution Done")
---
+++ Day 19 (Break and Continue Statement)/continue statement practical.py
@@ -0,0 +1,8 @@
+# continue statement Example :-
+
+for i in range(1,10):
+    if (i == 5):
+        # i = 5 will be skipped, and continue the loop for i = 6.
+        continue
+
+    print("i = ",i)
