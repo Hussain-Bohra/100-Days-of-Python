fd14b4beaeb4ed0899e7b9a970bfdaabea09adf3
Python Developer <PythonDeveloper@gmail.com>
2022-12-26 15:22:59 +0530

    fstring in Python

---
+++ Day 26 (fstrings in Python)/fstring practical.py
@@ -0,0 +1,17 @@
+# fstring example :-
+name = input("Enter Your Name: ").title()
+age = int(input("Enter Your Age: "))
+
+print("Details:-")
+print(f"My Name is {name} and I am {age} years old.")
+print()
+
+# Perform calculations using fstring, but it will return it as a string.
+# Example :-
+# print("Addition = ",end="")
+# print(f"{2 + 4}")
+# print(type(f"{2 + 4}")) # we can check the type also.
+
+# print("Subtraction = ",end="")
+# print(f"{2 * 4}")
+# print(type(f"{2 * 4}")) # we can check the type also.
